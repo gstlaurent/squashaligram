c65625cbf57f4a1d051c417eed3676bcde41b05f
AlisonClark <alison.marie.clark@gmail.com>
2014-10-25 15:41:27 -0700

    minor changes

--- squash.hs
+++ squash.hs
@@ -1,30 +1,31 @@
 -- board_string is the current board as a string, history_strings is a list of
 -- all the previous boards as strings
 crusher :: [String] -> Char -> Int -> Int -> [String]
-crusher (board_string:history_strings) player depth size = 
+crusher (board_string:history_strings) player depth n = 
    unparse (best_next_move board player depth history):board_string:history_strings
-   where board = parse size board_string
-         history = map (parse size) history_strings
+   where board = parse n board_string
+         history = map (parse n) history_strings
 
 -- converts input string to our board representation
 parse :: Int -> String -> Board
-parse size string = Board { whites = filterPlayer 'W' positions,
+parse n string = Board { whites = filterPlayer 'W' positions,
                             blacks = filterPlayer 'B' positions,
-                            size = size
+                            n = n
                             }
-   where positions = getAllPositions string size
+   where positions = getAllPositions string n
 
 filterPlayer :: Char -> [(Pos, Char)] -> [Pos]
 filterPlayer player positions = map fst $ filter is_player positions
    where is_player (pos,char) = char == player
 
-getAllPositions string size =
-   (getTopPositions string size) ++ (getBottomPositions string size)
+getAllPositions :: String -> Int -> [(Pos,Char)]   
+getAllPositions string n =
+   (getTopPositions string n) ++ (getBottomPositions string n)
 
-getTopPositions string size = concat getTopPositions' string 1 size
-getBottomPositions string size =
-   concat getBottomPositions' (drop top_length string) (size+1) size
-   where top_length = sum [size..(2*size-1)] -- sum of the first size rows
+getTopPositions string n = concat (getTopPositions' string 1 n)
+getBottomPositions string n =
+   concat (getBottomPositions' (drop top_length string) (n+1) n)
+   where top_length = sum [n..(2*n-1)]
                                       
 
 -- for the first n rows    
@@ -74,7 +75,7 @@ best_next_move board player depth history = board1 -- TODO
 
 data Board = Board {whites :: [Pos],
                     blacks :: [Pos],
-                    size :: Int
+                    n :: Int
                     }
              deriving (Show,Eq) 
 
@@ -92,4 +93,4 @@ pos2_2 = Pos2 {row = 2, col = 2}
 
 
 
-board1 = Board {whites = [pos1,pos2], blacks = [], size = 3}
+board1 = Board {whites = [pos1,pos2], blacks = [], n = 3}
